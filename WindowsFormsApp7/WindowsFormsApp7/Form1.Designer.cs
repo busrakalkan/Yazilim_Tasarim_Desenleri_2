5350807e5f0e70419a726e02f6f41a98282fc5bc
busrakalkan <kalkanbusra@outlook.com.tr>
2021-03-17 20:14:55 +0300

    tasarım desenleri denemeleri
    State, Template, Command ve Adaptör

---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.radioButton6);
+            this.groupBox3.Controls.Add(this.radioButton5);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(290, 198);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(225, 130);
+            this.groupBox3.TabIndex = 35;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "12:30";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(21, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(119, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(15, 70);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(14, 13);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(16, 38);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(14, 13);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(141, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Promosyon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ekonomik";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(29, 205);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(225, 123);
+            this.groupBox2.TabIndex = 34;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "07:45";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(18, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(107, 20);
+            this.textBox3.TabIndex = 42;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(18, 53);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(14, 13);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(18, 23);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(14, 13);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ekonomik";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Promosyon";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(290, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 139);
+            this.groupBox1.TabIndex = 33;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Müşteri Bilgileri";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(106, 105);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Öğrenci Bilet";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 105);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Tam Bilet";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "müşteri \r\nkaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(564, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 63);
+            this.button2.TabIndex = 41;
+            this.button2.Text = "sefer\r\nkaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 153);
+            this.pictureBox1.TabIndex = 42;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "indir.jpg");
+            this.ımageList1.Images.SetKeyName(1, "images.jpg");
+            this.ımageList1.Images.SetKeyName(2, "indir (2).jpg");
+            this.ımageList1.Images.SetKeyName(3, "images (1).jpg");
+            this.ımageList1.Images.SetKeyName(4, "indir (1).jpg");
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 392);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        //state kullanımı
+        #region stateDeseni
+        public interface HesapDurumu
+        {
+            int ucret();
+            string aciklama();
+        }
+        public class TamBilet : HesapDurumu
+        {
+            public int ucret()
+            {
+                return 30;
+            }
+            public string aciklama()
+            {
+                return "Tam";
+            }
+        }
+        public class OgrenciBilet : HesapDurumu
+        {
+            public int ucret()
+            {
+                return 25;
+            }
+            public string aciklama()
+            {
+                return "Öğrenci";
+            }
+        }
+        public class Hesap
+        {
+
+            private HesapDurumu hesapDurumu;
+
+            public Hesap()
+            {
+                hesapDurumu = new TamBilet();
+            }
+
+            public int ödeme()
+            {
+                return hesapDurumu.ucret();
+            }
+            public string aciklama()
+            {
+                return "bilet seçimi:" + hesapDurumu.aciklama();
+
+            }
+
+            public void durumDeğiştir(HesapDurumu hesapDurumu)
+            {
+                this.hesapDurumu = hesapDurumu;
+            }
+        } 
+        #endregion
+
+        //template kullanımı
+
+        enum BiletTipi
+        {
+            Promosyon, Ekonomi
+        }
+        abstract class Alisveris
+        {
+            protected BiletTipi biletTipi;
+            void Bitir() => MessageBox.Show($"{biletTipi}  tipi bilet seçildi");
+            abstract public void Sefer();
+            public void TemplateMethod()
+            {
+                Sefer();
+                Bitir();
+            }
+        }
+        class PromosyonBilet : Alisveris
+        {
+            public override void Sefer()
+            {
+                biletTipi = BiletTipi.Promosyon;
+                
+            }
+        }
+        class EcoBilet : Alisveris
+        {
+            public override void Sefer()
+            {
+                biletTipi = BiletTipi.Ekonomi;
+            }
+        }
+
+       //command kullanımı
+        public class Kayıt
+        {
+            public int TelNum { get; set; }
+            public string AdSoyad { get; set; }
+            
+        }
+        public class ReceiverKayıt
+        {
+            private Kayıt _EntityKayıt;
+
+            public ReceiverKayıt(Kayıt entityKayıt)
+            {
+                this._EntityKayıt = entityKayıt;
+            }
+
+            public void Ekle()
+            {
+                MessageBox.Show($"{_EntityKayıt.AdSoyad}  İsimli Kullanıcı Eklendi.");
+            }
+
+            public void Sil()
+            {
+                MessageBox.Show($"Telefon Numarası:{_EntityKayıt.TelNum} Olan Müşteri Silindi.");
+            }
+        }
+        public abstract class CommandKayıt
+        {
+            protected ReceiverKayıt _receiverKayıt;
+            public CommandKayıt(ReceiverKayıt receiverKayıt)
+            {
+                this._receiverKayıt = receiverKayıt;
+            }
+
+            public abstract void Execute();
+        }
+        public class ConcreteCommandKayıtEkle : CommandKayıt
+        {
+            public ConcreteCommandKayıtEkle(ReceiverKayıt receiverKayıt)
+                : base(receiverKayıt)
+            {
+
+            }
+
+            public override void Execute()
+            {
+                _receiverKayıt.Ekle();
+            }
+        }
+        public class ConcreteCommandKayıtSil : CommandKayıt
+        {
+            public ConcreteCommandKayıtSil(ReceiverKayıt receiverKayıt)
+                : base(receiverKayıt)
+            {
+
+            }
+
+            public override void Execute()
+            {
+                _receiverKayıt.Sil();
+            }
+        }
+        public class InvokerKayıt
+        {
+            public List<CommandKayıt> CommandKayıtList { get; set; }
+
+            public InvokerKayıt()
+            {
+                CommandKayıtList = new List<CommandKayıt>();
+            }
+
+            public void ExecuteAll()
+            {
+                if (CommandKayıtList.Count == 0)
+                    return;
+
+                foreach (CommandKayıt item in CommandKayıtList)
+                {
+                    item.Execute();
+                }
+            }
+        }
+
+        //adaptör kullanımı
+        public interface DenizOtobüsü
+        {
+            void feribot();
+            string gemitipi();
+        }
+        public interface ArabalıFeribot
+        {
+            void arabalıferibot();
+            string gemitipi();
+        }
+        public class DO1145 : DenizOtobüsü
+        {
+            public void feribot()
+            {
+                MessageBox.Show("deniz otobüsü seçildi");
+            }
+
+            public string gemitipi()
+            {
+                return "Deniz Otobüsü";
+            }
+
+        }
+        public class AF745 : ArabalıFeribot
+        {
+            public void arabalıferibot()
+            {
+                MessageBox.Show("arabalı feribot seçildi");
+            }
+
+            public string gemitipi()
+            {
+                return "Arabalı Feribot";
+            }
+
+        }
+        public class AfAdapter : DenizOtobüsü
+        {
+            ArabalıFeribot turkey;
+
+
+            public AfAdapter(ArabalıFeribot turkey)
+            {
+                this.turkey = turkey;
+            }
+
+            public void feribot()
+            {
+                turkey.arabalıferibot();
+            }
+
+            public string gemitipi()
+            {
+                return turkey.gemitipi();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 2000;
+            timer1.Enabled = true;
+            pictureBox1.Image = ımageList1.Images[0];
+        }
+
+ 
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Hesap hesap = new Hesap();
+            int ıd = Convert.ToInt32(textBox2.Text.ToString());
+            string c = textBox1.Text;
+
+            Kayıt Kisi = new Kayıt { TelNum = ıd, AdSoyad = c };
+            ReceiverKayıt rk1 = new ReceiverKayıt(Kisi);
+            CommandKayıt ckAdd = new ConcreteCommandKayıtEkle(rk1);
+            InvokerKayıt ik = new InvokerKayıt();
+            ik.CommandKayıtList.Add(ckAdd);
+            ik.ExecuteAll();
+
+            AF745 arbl = new AF745();
+            DenizOtobüsü AfAdapter = new AfAdapter(arbl);
+            textBox3.Text = arbl.gemitipi();
+            DO1145 do11 = new DO1145();
+            textBox4.Text = do11.gemitipi();
+            
+
+            if (radioButton1.Checked)
+            {
+                int a,b;
+                a = hesap.ödeme()-1;
+                b = a + 2;
+                radioButton3.Text = a.ToString();
+                radioButton5.Text = a.ToString();
+
+                radioButton4.Text = b.ToString();
+                radioButton6.Text = b.ToString();
+
+            }
+
+            if (radioButton2.Checked)
+            {
+                hesap.durumDeğiştir(new OgrenciBilet());
+                int a, b;
+                a = hesap.ödeme() - 1;
+                b = a + 2;
+                radioButton3.Text = a.ToString();
+                radioButton5.Text = a.ToString();
+
+                radioButton4.Text = b.ToString();
+                radioButton6.Text = b.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           if(radioButton3.Checked)
+            {
+                MessageBox.Show($"{groupBox2.Text} sefer saatli {textBox3.Text}"+ "\n" +$"{label1.Text} tipi " + "\n" + $"{radioButton3.Text} tl ücretli bilet kaydedildi");
+            }
+            if (radioButton4.Checked)
+            {
+                MessageBox.Show($"{groupBox2.Text} sefer saatli {textBox3.Text}" + "\n" + $"{label2.Text} tipi " + "\n" + $"{radioButton4.Text} tl ücretli bilet kaydedildi");
+            }
+            if (radioButton5.Checked)
+            {
+                MessageBox.Show($"{groupBox3.Text} sefer saatli {textBox4.Text}" + "\n" + $"{label2.Text} tipi " + "\n" + $"{radioButton5.Text} tl ücretli bilet kaydedildi");
+            }
+            if (radioButton6.Checked)
+            {
+                MessageBox.Show($"{groupBox3.Text} sefer saatli {textBox4.Text}" + "\n" + $"{label2.Text} tipi " + "\n" + $"{radioButton6.Text} tl ücretli bilet kaydedildi");
+            }
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            Alisveris a1 = new PromosyonBilet();
+            a1.TemplateMethod();
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            Alisveris a1 = new PromosyonBilet();
+            a1.TemplateMethod();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            Alisveris a1 = new EcoBilet();
+            a1.TemplateMethod();
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            Alisveris a1 = new EcoBilet();
+            a1.TemplateMethod();
+        }
+
+        int i = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+            if (i == 5)
+                i = 0;
+            pictureBox1.Image = ımageList1.Images[i];
+        }
+    }
+    
+}
